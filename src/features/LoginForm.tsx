14e5b51e0e7763e1e66c31b052b1c216972a2ac4
Alexander Golyshev <youcanwinmyheart@gmail.com>
2024-11-16 16:23:04 +0300

    fix

--- src/features/LoginForm.tsx
+++ src/features/LoginForm.tsx
@@ -19,9 +19,7 @@ const formSchema = z.object({
     email: z.string().email({
         message: "Invalid email address"
     }),
-    password: z.string().min(8,{
-        message: "Password must be at least 8 characters"
-    }),
+    password: z.string(),
 })
 
 export function ProfileForm() {
