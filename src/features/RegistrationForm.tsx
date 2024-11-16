a7a9d565b210dfde496317926d3507c6b481b612
Alexander Golyshev <youcanwinmyheart@gmail.com>
2024-11-16 16:20:01 +0300

    fix

--- src/features/RegistrationForm.tsx
+++ src/features/RegistrationForm.tsx
@@ -20,9 +20,7 @@ const formSchema = z.object({
     username: z.string().min(2, {
         message: "Username must be at least 2 characters.",
     }),
-    password: z.string().min(8,{
-        message: "Password must be at least 8 characters"
-    }),
+    password: z.string(),
     email: z.string().email({
         message: "Invalid email address"
     }),
